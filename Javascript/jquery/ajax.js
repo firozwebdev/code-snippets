d015820486209789241468ac5c1020a9e41c36eb
sabuzwebdev <frsmgt@gmail.com>
2018-07-24 19:20:09 +0600

    Ajax method is updated

--- Javascript/jquery/ajax.js
+++ Javascript/jquery/ajax.js
@@ -30,4 +30,40 @@ var request = $.ajax({
     alert( "Request failed: " + textStatus );
   });
 
-  
+
+  //When ajax metod is used for Laravel, You could use either $.post() method or $.ajax() method
+
+var person = {
+    name : $('#name').val(),
+    email : $('#email').val(),
+    address : $('#address').val(),
+};
+
+var data = {
+    _token : _token,  //CSRF field will generate token automatically
+    person: person
+};
+
+$.post("Url", data, function(res, status){  // passing data to the server 
+    console.log(res);
+});
+
+//Or
+
+
+var request = jQuery.ajax({
+    url: "{{ route('create.company') }}",  //route or Url to connect with laravel server
+    data: data,
+    method: "POST",
+    dataType: "json"
+});
+request.done(function (response) {
+    console.log(response);
+})
+
+request.fail(function (jqXHT, textStatus) {
+    $.notify(textStatus, "error");
+});
+
+
+
